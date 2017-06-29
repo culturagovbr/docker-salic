4874949b5fab5c4a1520463c3a1415fe7ca1c576
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-06-28 22:25:34 -0300

    [UPDATE] Ajustes no Entrypoint tratando os diretórios e copiando o arquivo sample "exemplo-application.ini" para "application.ini"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
-
-if ! [ -e index.php ] || ! [ -n "$(ls -A /var/www/salic)" ] || ! [ -n "$(ls -A /var/www/salic/application)" ]; then
-    echo "Application not found in /var/www/salic - copying now..."
+echo "[ ****************** ] Starting Endpoint of Application"
+if ! [ -d "/var/www/salic" ] || ! [ -d "/var/www/salic/application" ]; then
+    echo "Application not found in /var/www/salic - cloning now..."
     if [ "$(ls -A)" ]; then
         echo "WARNING: /var/www/salic is not empty - press Ctrl+C now if this is an error!"
         ( set -x; ls -A; sleep 10 )
     fi
-
+    echo "[ ****************** ] Cloning Project repository to tmp folder"
     git clone -b 'develop' http://git.cultura.gov.br/sistemas/novo-salic.git /tmp/salic
     ls -la /tmp/salic
 
+    echo "[ ****************** ] Copying Project from temporary folder to workdir"
     cp /tmp/salic -r /var/www
+
+    echo "[ ****************** ] Copying sample application configuration to real one"
+    cp /var/www/salic/application/configs/exemplo-application.ini /var/www/salic/application/configs/application.ini
+
+    echo "[ ****************** ] Changing owner and group from the Project to 'www-data' "
     chown www-data:www-data -R /var/www/salic
-    echo "Complete! The application has been successfully copied to /var/www/salic"
 
     ls -la /var/www/salic
-fi
 
+    echo "Complete! The application has been successfully copied to /var/www/salic"
+fi
+echo "[ ****************** ] Ending Endpoint of Application"
 exec "$@"
