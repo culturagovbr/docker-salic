1736af0a1823527b1d5a3339727af2b4b579f24c
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-07-04 21:30:46 -0300

    [UPDATE] Adicionando download e instalação do composer dentro do container.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,7 +22,15 @@ if ! [ -d "/var/www/salic" ] || ! [ -d "/var/www/salic/application" ]; then
 
     ls -la /var/www/salic
 
-    echo "Complete! The application has been successfully copied to /var/www/salic"
+
+    echo "[ ****************** ] Downloading composer "
+
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+    echo "[ ****************** ] Installing composer "
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
 fi
 echo "[ ****************** ] Ending Endpoint of Application"
 exec "$@"
